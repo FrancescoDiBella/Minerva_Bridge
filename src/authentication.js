74304bf8b893ce274b9bb59f1aa823a8762b7e4e
FrancescoDiBella <francescino10401@gmail.com>
2023-06-16 17:28:16 +0200

    Eliminato file deprecato

--- src/authentication.js
+++ src/authentication.js
@@ -1,7 +1,6 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
-//const CustomStrategy = require('./custom-strategy');
 const { JwtService } = require('./services/jwt-service/jwt-service.class');
 
 module.exports = app => {
