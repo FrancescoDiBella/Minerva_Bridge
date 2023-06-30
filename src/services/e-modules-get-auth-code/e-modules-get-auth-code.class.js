a3a0822b8da852a0504b2a8986f6b409c0a524c7
FrancescoDiBella <francescino10401@gmail.com>
2023-06-30 09:33:52 +0200

    Aggiunto service e-modules/getAuthCode

--- src/services/e-modules-get-auth-code/e-modules-get-auth-code.class.js
+++ src/services/e-modules-get-auth-code/e-modules-get-auth-code.class.js
@@ -1,36 +1,62 @@
-/* eslint-disable no-unused-vars */
+const { Service } = require('feathers-sequelize');
+const getAuth = require('../../models/get-auth-code.model');
+const utenti = require('../../models/access.model');
+const crypto = require('crypto');
+const { BadRequest } = require('@feathersjs/errors');
+
 exports.EModulesGetAuthCode = class EModulesGetAuthCode {
-  constructor (options) {
-    this.options = options || {};
+  constructor(options, app){
+    this.options = options;
+    this.app = app;
   }
 
-  async find (params) {
-    return [];
-  }
+  async create(data, params){
+    const { idApp3D } = data;
+    if(idApp3D == null || idApp3D == undefined || idApp3D == "" || idApp3D == " "){
+      throw new BadRequest("Non è stato fornito l'idApp3D.")
+    }
+    const getAuthModel = getAuth(this.app);
+    const utentiModel = utenti(this.app);
 
-  async get (id, params) {
-    return {
-      id, text: `A new message with ID: ${id}!`
-    };
-  }
+    //Check if user exists;
+    const user = await utentiModel.findOne({
+      where: {
+        idApp3D: idApp3D
+      }
+    });
 
-  async create (data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
+    if(!user){
+      throw new BadRequest("Non c'è nessun utente associato all'App3D o non esiste nessun App3D con tale id.");
     }
 
-    return data;
+    const code = this.generateAuth();
+
+    await getAuthModel.create({
+      idApp3D,
+      authCode:code.toString(),
+      validated: false
+    })
+
+    this.deleteAuthCode(code.toString(), idApp3D)
+
+    return {authCode:code};
   }
 
-  async update (id, data, params) {
-    return data;
+  deleteAuthCode(authCode, idApp3D){
+    setTimeout(()=>{
+      const getAuthModel = getAuth(this.app);
+      getAuthModel.destroy({
+        where :{
+          idApp3D,
+          authCode,
+          validated: false
+        }
+      })
+      console.log("Eliminato")
+    }, 10000*6)
   }
 
-  async patch (id, data, params) {
-    return data;
-  }
-
-  async remove (id, params) {
-    return { id };
+  generateAuth(){
+    return crypto.randomBytes(2).toString('hex');
   }
 };
--- src/services/e-modules-get-auth-code/e-modules-get-auth-code.hooks.js
+++ src/services/e-modules-get-auth-code/e-modules-get-auth-code.hooks.js
@@ -1,14 +1,44 @@
-
+const internalOnly = require('../../internal-only');
+const jwt = require('jsonwebtoken');
+const {BadRequest} = require('@feathersjs/errors');
+const { NotAuthenticated } = require('@feathersjs/errors')
 
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    find: [internalOnly],
+    get: [internalOnly],
+    create:[
+      async context => {
+        const { headers } = context.params;
+        console.log("DATA:", context.data)
+        const data = context.data;
+        // Check if the `Authorization` header is present
+        if (!headers.authorization) {
+          throw new BadRequest('Manca l\'header `Authorization`');
+        }
+
+        // Extract the JWT from the `Authorization` header
+        const [, token] = headers.authorization.split(' ');
+
+        // Verify the JWT using the secret key
+        try {
+          const secret = context.app.get('authentication').secret;
+          const payload = jwt.verify(token, secret);
+          data.idLms = payload.idLms.toString();
+          data.secret = payload.secret;
+          context.data = data;
+          return context;
+
+        } catch (error) {
+          // If the JWT is invalid, throw an error
+          throw new NotAuthenticated('Token non valido!');
+        }
+      }
+    ],
+    update: [internalOnly],
+    patch: [internalOnly],
+    remove: [internalOnly]
   },
 
   after: {
