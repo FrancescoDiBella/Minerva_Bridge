0b404777d7e13e63776a5e57ee6c0360955ac219
FrancescoDiBella <francescino10401@gmail.com>
2023-06-30 10:50:05 +0200

    modificato hook

--- src/services/store-save-data/store-save-data.hooks.js
+++ src/services/store-save-data/store-save-data.hooks.js
@@ -1,13 +1,42 @@
-const internalOnly = require('../../internal-only');
 const jwt = require('jsonwebtoken');
-const {BadRequest} = require('@feathersjs/errors');
+const internalOnly = require('../../internal-only');
 
 module.exports = {
   before: {
     all: [],
     find: [internalOnly],
     get: [internalOnly],
-    create: [],
+    create: [
+      async context => {
+        const { headers } = context.params;
+        //console.log("DATA:", context.data)
+        const save_data = context.data.save_data;
+        // Check if the `Authorization` header is present
+        if (!headers.authorization) {
+          throw new Error('Missing `Authorization` header');
+        }
+
+        // Extract the JWT from the `Authorization` header
+        const [, token] = headers.authorization.split(' ');
+
+        // Verify the JWT using the secret key
+        try {
+          const secret = context.app.get('authentication').secret;
+          const payload = jwt.verify(token, secret);
+
+          const clientData = {
+            save_data,
+            payload
+          }
+          context.data = clientData;
+          return context;
+
+        } catch (error) {
+          // If the JWT is invalid, throw an error
+          throw new Error('Invalid token');
+        }
+      }
+    ],
     update: [internalOnly],
     patch: [internalOnly],
     remove: [internalOnly]
