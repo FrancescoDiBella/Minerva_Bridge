26762f96a95dee002a8598305fbcb256213dcc57
FrancescoDiBella <francescino10401@gmail.com>
2023-06-30 10:49:47 +0200

    modificato hook

--- src/services/statements/statements.hooks.js
+++ src/services/statements/statements.hooks.js
@@ -1,14 +1,46 @@
+const jwt = require('jsonwebtoken');
+const internalOnly = require('../../internal-only');
 
 
 module.exports = {
   before: {
     all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    find: [internalOnly],
+    get: [internalOnly],
+    create: [
+      async context => {
+        const { headers } = context.params;
+        //console.log("DATA:", context.data)
+        const save_data = context.data.save_data;
+        // Check if the `Authorization` header is present
+        if (!headers.authorization) {
+          throw new Error('Missing `Authorization` header');
+        }
+
+        // Extract the JWT from the `Authorization` header
+        const [, token] = headers.authorization.split(' ');
+
+        // Verify the JWT using the secret key
+        try {
+          const secret = context.app.get('authentication').secret;
+          const payload = jwt.verify(token, secret);
+
+          const clientData = {
+            save_data,
+            payload
+          }
+          context.data = clientData;
+          return context;
+
+        } catch (error) {
+          // If the JWT is invalid, throw an error
+          throw new Error('Invalid token');
+        }
+      }
+    ],
+    update: [internalOnly],
+    patch: [internalOnly],
+    remove: [internalOnly]
   },
 
   after: {
