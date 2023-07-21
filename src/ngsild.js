d98855165e41f41808ff605ae9ae5aff60682ed1
FrancescoDiBella <francescino10401@gmail.com>
2023-07-21 16:21:51 +0200

    Aggiunta classe di supporto per la generazine di entità NGSI-LD

---
+++ src/ngsild.js
@@ -0,0 +1,10 @@
+
+exports.ngsild = class NGSILD{
+  constructor (id, type, attributes, relationships, context) {
+    this.id = id;
+    this.type = type;
+    this.attributes = attributes;
+    this.relationships = relationships;
+    this.context = context;
+  }
+}
