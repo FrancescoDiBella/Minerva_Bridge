9b57aedffef47f6c55f90a2dffd40c947b15a3f2
FrancescoDiBella <francescino10401@gmail.com>
2023-07-26 16:50:32 +0200

    Modificata classe ausiliaria NGSILD

--- src/ngsild.js
+++ src/ngsild.js
@@ -1,10 +1,51 @@
 
 exports.ngsild = class NGSILD{
-  constructor (id, type, attributes, relationships, context) {
+  constructor (id, type, properties, relationships, context) {
     this.id = id;
     this.type = type;
-    this.attributes = attributes;
+    this.properties = properties;
     this.relationships = relationships;
     this.context = context;
+  }
+
+  // Generare entità NGSILD
+  generateEntity(){
+    var entity = {
+      "id": this.id,
+      "type": this.type,
+      "@context": this.context
+    }
+
+    for(let i = 0; i < this.properties.length; i++){
+      const prop = this.properties[i];
+      entity[prop.name] = prop.value;
+    }
+
+    for(let i = 0; i < this.relationships.length; i++){
+      const rel = this.relationships[i];
+      entity[rel.name] = rel.value;
+    }
+
+    /*
+      // Esempio di proprietà
+      const color = {
+        name : "color",
+        value : {
+          "type": "Property",
+          "value": "red"
+        }
+      }
+
+      //Esempio di relazione
+      const hasObject = {
+        name : "hasObject",
+        value : {
+          "type": "Relationship",
+          "object": "urn:ngsi-ld:Building:001"
+        }
+      }
+    */
+
+    return entity;
   }
 }
