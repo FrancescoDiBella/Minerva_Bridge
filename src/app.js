8502a072a35503d7cc9c81dfa516f37582020b10
FrancescoDiBella <francescino10401@gmail.com>
2023-07-28 15:54:03 +0200

    Eliminato blocco di codice commentato superfluo

--- src/app.js
+++ src/app.js
@@ -41,26 +41,6 @@ app.configure(express.rest());
 app.configure(socketio());
 
 app.configure(sequelize);
-
-/*
-const entities = {
-  lms: require('./models/lms.model'),
-  utenti: require('./models/access.model'),
-  saves: require('./models/store-save-datas.model'),
-  auth: require('./models/get-auth-code.model')
-}
-
-entities.lms.hasMany(entities.utenti, {foreignKey: 'idLms'});
-entities.utenti.belongsTo(entities.lms);
-entities.utenti.hasMany(entities.saves, {foreignKey: 'id_utenza'});
-entities.saves.belongsTo(entities.utenti);
-
-sequelize.sync({force: false}).then(() => {
-  console.log("Database synced");
-}).catch((err) => {
-  console.log(err);
-});
-*/
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
 app.configure(authentication);
