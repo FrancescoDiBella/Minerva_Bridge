eef3265baf58191405895fa9b55a6384a0836143
FrancescoDiBella <francescino10401@gmail.com>
2023-07-28 10:53:11 +0200

    Aggiunto controllo su "score" e "progress"

--- src/services/statements/statements.class.js
+++ src/services/statements/statements.class.js
@@ -10,7 +10,24 @@ exports.Statements = class Statements {
   constructor (options, app) {
     this.options = options || {};
     this.app = app;
+    this.lesson_status = ["passed", "completed", "failed", "incomplete", "browsed", "not attempted"];
+    this.scormMap = {
+      "score": "cmi.core.score.raw",
+      "masteryscore": "adlcp:masteryscore",
+      "mastery_score": "cmi.student_data.mastery_score",
+      "launch_data": "cmi.launch_data",
+      "suspend_data": "cmi.suspend_data",
+      "lesson_location": "cmi.core.lesson_location",
+      "progress": "cmi.core.lesson_status",
+      "entry": "cmi.core.entry",
+      "exit": "cmi.core.exit",
+      "time": "cmi.core.total_time",
+      "session_time": "cmi.core.session_time",
+      "student_id": "cmi.core.student_id",
+      "student_name": "cmi.core.student_name"
     }
+    this.maxAcceptableErrors = 2;
+  }
 
   async find (params) {
     return [];
@@ -213,16 +230,22 @@ exports.Statements = class Statements {
     return false;
   }
 
-  async isSCORMValueValid(data){
-    for(let i = 0; i < data.length; i++){
-      const {identifier, parameter, value, timestamp} = data[i];
-      if(identifier == "defaultplayer"){
-        if(parameter == "cmi.core.score.raw" || parameter == "adlcp:masteryscore" || parameter == "cmi.student_data.mastery_score"){
-          if(value == null || value == undefined || value == "" || value == " "){
-            return false;
-          }
+  async isSCORMValueValid(parameter, value){
+    switch(parameter){
+      case "score":
+      case "masteryscore":
+      case "mastery_score":
+        if(!this.isNumber(value)){
+          return false;
         }
-      }
+        break;
+      case "progress":
+        if(!this.lesson_status.includes(value)){
+          return false;
+        }
+        break;
+      default:
+        break;
     }
     return true;
   }
@@ -286,20 +309,39 @@ exports.Statements = class Statements {
           {
               "element":"cmi.core.student_id",
               "value":idUsr
+          },
+          {
+              "element":"cmi.core.student_name",
+              "value":null
           },]
     };
 
+    var isNotAccepted = 0;
     for(let i = 0; i < data.length; i++){
       const {identifier, parameter, value, timestamp} = data[i];
-      if(identifier == "defaultplayer"){
-        for(let j = 0; j < scorm.data.length; j++){
-          if(scorm.data[j].element == parameter){
-            scorm.data[j].value = value;
+      if(identifier != "defaultplayer"){
+        continue;
+      }
+      scorm.data[scorm.data.length - 1].value = "defaultplayer";
+      for(let j = 0; j < scorm.data.length; j++){
+        const _param = this.scormMap[parameter];
+        if(_param == undefined){
+          continue;
+        }
+
+        if(scorm.data[j].element == _param){
+          if(!this.isSCORMValueValid(_param, value)){
+            isNotAccepted++;
+            continue;
           }
+          scorm.data[j].value = value;
         }
       }
     }
 
+    if(isNotAccepted >= this.maxAcceptableErrors){
+      return null;
+    }
     return scorm;
   }
   async sendSCORMData(statement, baseURL, postfix, authToken, key, secret){
@@ -350,6 +392,8 @@ exports.Statements = class Statements {
       return BadRequest("Errore, Statements non salvati! Dettagli: "  + err);
     }
   }
+
+  async isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
   async generateNGSILD(data, idUsr, idLms, idApp3D, authCode){
     //genera NGSILD
