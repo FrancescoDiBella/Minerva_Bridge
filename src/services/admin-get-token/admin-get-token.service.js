25355079c3f2fcc8064ff1d38ed2ddbfbee80565
Marco Cavalli <cmarcos46@gmail.com>
2023-09-04 10:30:39 +0200

    feat: swagger definitions for /admin/getToken

--- src/services/admin-get-token/admin-get-token.service.js
+++ src/services/admin-get-token/admin-get-token.service.js
@@ -1,17 +1,62 @@
 // Initializes the `adminGetToken` service on path `/admin/getToken`
-const { AdminGetToken } = require('./admin-get-token.class');
-const hooks = require('./admin-get-token.hooks');
+const { AdminGetToken } = require("./admin-get-token.class");
+const hooks = require("./admin-get-token.hooks");
 
 module.exports = function (app) {
   const options = {
-    paginate: app.get('paginate')
+    paginate: app.get("paginate"),
+  };
+  const AdminGetTokenService = new AdminGetToken(options, app);
+  AdminGetTokenService.docs = {
+    operations: {
+      find: false,
+      create: {
+        summary: "Genera un token di autenticazione per un utente admin.",
+        description:
+          "Endpoint che permette di ottenere un token valido per le richieste successive alla registrazione dell’admin quali: \
+          POST /admin/lms/:id/user \
+          Se l’admin che richiede il token è il superadmin allora tale token permette di richiamare ad esempio: \
+          GET /admin (lista di tutti gli utenti admin) \
+          Insieme al token vengono restituiti tempo emissione e tempo per la scadenza, il token è valido per un minuto.",
+        responses: {
+          201: {
+            description: "Token di autenticazione valido per un minuto.",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    token: {
+                      type: "string",
+                      example: "jK8sL9nRmT4oP5qA2cE7...",
+                    },
+                    iat: {
+                      type: "integer",
+                      example: 1693816119,
+                    },
+                    exp: {
+                      type: "integer",
+                      example: 1693816419,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          400: {
+            description: "Utente admin non registrato o password errata.",
+            summary: "Utente admin non registrato.",
+          },
+        },
+      },
+    },
   };
 
   // Initialize our service with any options it requires
-  app.use('/admin/getToken', new AdminGetToken(options, app));
+  app.use("/admin/getToken", AdminGetTokenService);
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('admin/getToken');
+  const service = app.service("admin/getToken");
 
   service.hooks(hooks);
 };
