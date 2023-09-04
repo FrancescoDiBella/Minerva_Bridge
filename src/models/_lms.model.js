74f5505adf97af809177af9b9b94796e82e21808
Marco Cavalli <cmarcos46@gmail.com>
2023-09-04 16:52:03 +0200

    chore: comment console.log output

--- src/models/_lms.model.js
+++ src/models/_lms.model.js
@@ -49,7 +49,7 @@ module.exports = function (app) {
   lms.sync({ alter: true });
   // eslint-disable-next-line no-unused-vars
   lms.associate = function (models) {
-    console.log(models);
+    // console.log(models);
     lms.hasMany(models.utenti, { foreignKey: "idLms" });
     lms.belongsTo(models.admins, { foreignKey: "idAdmin" });
   };
