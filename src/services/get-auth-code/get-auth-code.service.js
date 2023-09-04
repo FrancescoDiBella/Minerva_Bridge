04afa29cb4ffb47f80c8ac12d1608aacec3e4829
Marco Cavalli <cmarcos46@gmail.com>
2023-09-04 14:39:10 +0200

    feat: openapi definition for POST /3d-modules/getAuthCode

--- src/services/get-auth-code/get-auth-code.service.js
+++ src/services/get-auth-code/get-auth-code.service.js
@@ -9,8 +9,73 @@ module.exports = function (app) {
     paginate: app.get("paginate"),
   };
 
+  const GetAuthCodeService = new GetAuthCode(options, app);
+
+  GetAuthCodeService.docs = {
+    summary: "Genera un codice di autenticazione utente per app 3D.",
+    description: "Genera un codice di autenticazione utente per app 3D.",
+    operations: {
+      find: false,
+      get: false,
+      create: {
+        summary: "Genera un codice di autenticazione utente per app 3D.",
+        description:
+          "A fronte di un idApp3D valido (si veda chiamata admin/lms/:idLms/user), \
+                      viene generato un authCode univoco, una stringa randomica per l’app3D  \
+                      (tale authCode rimane pendente fin quando non viene validato), dopo un certo \
+                      periodo di tempo se l’authCode non è stato redento esso viene invalidato. Viene \
+                      restituito tale authCode. Il bridge mantiene in tabella una associazione fra idApp3D \
+                      e authCode generato, attendendo che venga redento/utilizzato.",
+        bodyRequest: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  idApp3D: {
+                    type: "string",
+                    example: "12",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "AuthCode generato con successo.",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    authCode: {
+                      type: "string",
+                      example: "e7aa",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          400: {
+            description: "idApp3D non valido o mancante.",
+          },
+          404: {
+            description:
+              "Non c'è nessun utente associato all'App3D o non esiste nessun App3D con tale id.",
+          },
+        },
+      },
+      update: false,
+      patch: false,
+      remove: false,
+    },
+  };
+
   // Initialize our service with any options it requires
-  app.use("/3d-modules/getAuthCode", new GetAuthCode(options, app));
+  app.use("/3d-modules/getAuthCode", GetAuthCodeService);
 
   // Get our initialized service so that we can register hooks
   const service = app.service("3d-modules/getAuthCode");
