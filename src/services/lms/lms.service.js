e4893bcba67fe7ac256f4128e3ebc434b0f58571
Marco Cavalli <cmarcos46@gmail.com>
2023-09-04 11:21:08 +0200

    feat: add Swagger definitions for POST /admin/lms

--- src/services/lms/lms.service.js
+++ src/services/lms/lms.service.js
@@ -9,8 +9,106 @@ module.exports = function (app) {
     paginate: app.get("paginate"),
   };
 
+  const lmsService = new Lms(options, app);
+  lmsService.docs = {
+    summary: "Gestione degli LMS.",
+    description: "Gestione degli LMS.",
+    operations: {
+      find: false,
+      create: {
+        summary: "Registra un nuovo LMS.",
+        description:
+          "Richiamata da admin per creare nuovi LMS. \
+                      I parameters authUsername e authPassword sono opzionali, \
+                      essi sono le credenziali per poter invocare la chiamata di \
+                      salvataggio di XAPI/SCORM a baseURL.",
+        requestBody: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  name: {
+                    type: "string",
+                    example: "LMS TEST 1",
+                  },
+                  baseURL: {
+                    type: "string",
+                    example: "http://lrs:8080",
+                  },
+                  statementsType: {
+                    type: "string",
+                    example: "XAPI",
+                  },
+                  authUsername: {
+                    type: "string",
+                    example: "my_key",
+                  },
+                  authPassword: {
+                    type: "string",
+                    example: "my_secret",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "Admin creato con successo.",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    id: {
+                      type: "integer",
+                      example: 1,
+                    },
+                    name: {
+                      type: "string",
+                      example: "LMS TEST 1",
+                    },
+                    idAdmin: {
+                      type: "integer",
+                      example: 4,
+                    },
+                    updatedAt: {
+                      type: "string",
+                      example: "2021-09-01T09:00:00.000Z",
+                    },
+                    createdAt: {
+                      type: "string",
+                      example: "2021-09-01T09:00:00.000Z",
+                    },
+                  },
+                },
+              },
+            },
+          },
+          401: {
+            description: "Auth Token mancante o non valido!",
+          },
+          409: {
+            description: "LMS già esistente!",
+          }
+        },
+        security: [
+          {
+            bearerAuth: [],
+          }
+        ],
+      },
+      get: false,
+      update: false,
+      patch: false,
+      remove: false,
+    },
+  };
+
   // Initialize our service with any options it requires
-  app.use("/admin/lms", new Lms(options, app));
+  app.use("/admin/lms", lmsService);
 
   // Get our initialized service so that we can register hooks
   const service = app.service("/admin/lms");
