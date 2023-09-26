a6e4653ff4e78925b9b5f16864dc9b65bea73d66
FrancescoDiBella <francescino10401@gmail.com>
2023-09-26 16:07:06 +0200

    Added a service for testing NGSILD Notifications

---
+++ src/services/test-ngsild/test-ngsild.class.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+exports.TestNgsild = class TestNgsild {
+  constructor (options, app) {
+    this.options = options || {};
+    this.app = app;
+  }
+
+  async find (params) {
+    return [];
+  }
+
+  async get (id, params) {
+    return {
+      id, text: `A new message with ID: ${id}!`
+    };
+  }
+
+  async create (data, params) {
+    console.log("DATA:", data);
+
+    return data;
+  }
+
+  async update (id, data, params) {
+    return data;
+  }
+
+  async patch (id, data, params) {
+    return data;
+  }
+
+  async remove (id, params) {
+    return { id };
+  }
+};
